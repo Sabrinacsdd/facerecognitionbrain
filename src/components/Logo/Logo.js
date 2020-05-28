c42c3f16a93b6df361faa30af78d075e2508a8cf
sc <sabrina@hotmail.com>
2020-05-27 21:05:42 -0700

    check

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tilt from 'react-tilt';
+import './Logo.css';
+import Cat from './Cat.png';
+
+
+const Logo =() =>{
+    return (
+        <div className= 'ma4 mt0'>
+        <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 100, width: 100 }} >
+            <div className="Tilt-inner pa3"> 
+                <img style={{paddingRight:'10px'}} alt = 'Logo' src={Cat}></img> 
+            </div>
+        </Tilt>
+        </div>
+    );
+}
+export default Logo;
